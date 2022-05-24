d588ba8b25aff43bc3448ad5a6aacebeead1e6eb
Álex Albarca <alex@albarca.es>
2022-05-24 20:08:12 +0200

    Add the Whatsapp Text Message object

---
+++ src/Message/Message.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Netflie\WhatsAppCloudApi\Message;
+
+abstract class Message
+{
+    /**
+     * @var string Currently only "whatsapp" value is supported.
+     */
+    protected string $messaging_product = 'whatsapp';
+
+    /**
+     * @var string Currently only "individual" value is supported.
+     */
+    protected string $recipient_type = 'individual';
+
+    /**
+     * @var string WhatsApp ID or phone number for the person you want to send a message to.
+     */
+    protected string $to;
+
+    /**
+     * @var string Type of message object.
+     */
+    protected string $type;
+
+    /**
+     * Creates a new Message class.
+     *
+     * @param string         $to
+     */
+    public function __construct(string $to)
+    {
+        $this->to = $to;
+    }
+
+    /**
+     * Return the WhatsApp ID or phone number for the person you want to send a message to.
+     *
+     * @return string
+     */
+    public function to(): string
+    {
+        return $this->to;
+    }
+
+    /**
+     * Return the type of message object.
+     *
+     * @return string
+     */
+    public function type(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * Return the messaging product.
+     *
+     * @return string
+     */
+    public function messagingProduct(): string
+    {
+        return $this->messaging_product;
+    }
+
+    /**
+     * Return the recipient type.
+     *
+     * @return string
+     */
+    public function recipientType(): string
+    {
+        return $this->recipient_type;
+    }
+}
---
+++ src/Message/TextMessage.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Netflie\WhatsAppCloudApi\Message;
+
+class TextMessage extends Message
+{
+    /**
+     * @const int Maximum length for body message.
+     */
+    protected const MAXIMUM_LENGTH = 4096;
+
+    /**
+     * @var string Type of message object.
+     */
+    protected string $type = 'text';
+
+    /**
+     * @var string The body of the text message.
+     */
+    protected string $text;
+
+    /**
+     * @var bool Determines if show a preview box for URLs contained in the text message.
+     */
+    protected bool $preview_url;
+
+    /**
+     * Creates a new message of type text.
+     *
+     * @param string         $to
+     * @param string         $text
+     * @param bool           $preview_url
+     */
+    public function __construct(string $to, string $text, bool $preview_url = false)
+    {
+        $this->assertTextIsValid($text);
+
+        $this->text = $text;
+        $this->preview_url = $preview_url;
+
+        parent::__construct($to);
+    }
+
+    /**
+     * Return the body of the text message.
+     *
+     * @return string
+     */
+    public function text(): string
+    {
+        return $this->text;
+    }
+
+    /**
+     * Return if preview box for URLs contained in the text message is shown.
+     *
+     * @return bool
+     */
+    public function previewUrl(): bool
+    {
+        return $this->preview_url;
+    }
+
+    private function assertTextIsValid(string $text): void
+    {
+        if (strlen($text) > self::MAXIMUM_LENGTH) {
+            throw new \LengthException('The maximun length for a message text is ' . self::MAXIMUM_LENGTH . ' characters');
+
+        }
+    }
+}
