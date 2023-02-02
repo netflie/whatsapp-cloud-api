34513c1647d7e8fdd7f7d4f1187b5a370cc746f0
donmbelembe <donmbelembe@users.noreply.github.com>
2023-02-02 08:38:23 +0100

    rollback

--- src/WebHook/Notification/Order.php
+++ src/WebHook/Notification/Order.php
@@ -7,24 +7,31 @@ use Netflie\WhatsAppCloudApi\WebHook\Notification\Support\Products;
 final class Order extends MessageNotification
 {
     private string $catalog_id;
+    private string $message;
     private Products $products;
 
     public function __construct(
         string $id,
         Support\Business $business,
         string $catalog_id,
+        string $message,
         Products $products,
         string $received_at_timestamp
     ) {
         parent::__construct($id, $business, $received_at_timestamp);
 
         $this->catalog_id = $catalog_id;
+        $this->message = $message;
         $this->products = $products;
     }
 
     public function catalogId(): string
-    {
         return $this->catalog_id;
+    }
+
+    public function message(): string
+    {
+        return $this->message;
     }
 
     public function products(): Products
