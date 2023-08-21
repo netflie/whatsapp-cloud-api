de562975fde7860a770243d01bbe9b25a2e6992a
Ian Rothmann <ian@conceptsphere.com>
2023-08-21 17:11:26 +0200

    Footer fix, linting and tests
    Code linting, removing the "type" key from the footer, unit and integration tests that passes.

--- src/Message/ButtonReplyMessage.php
+++ src/Message/ButtonReplyMessage.php
@@ -4,41 +4,45 @@ namespace Netflie\WhatsAppCloudApi\Message;
 
 use Netflie\WhatsAppCloudApi\Message\ButtonReply\ButtonAction;
 
-class ButtonReplyMessage extends Message {
+class ButtonReplyMessage extends Message
+{
+    protected string $type = 'button';
 
-  protected string $type = 'button';
+    private ?string $header;
 
-  private ?string $header;
-  
-  private string $body;
+    private string $body;
 
-  private ?string $footer;
-  
-  private ButtonAction $action;
+    private ?string $footer;
 
-  public function __construct(string $to, string $body, ButtonAction $action, ?string $header = null, ?string $footer = null) {
-    $this->body = $body;
-    $this->action = $action;
-    $this->header = $header;
-    $this->footer = $footer;
-    
-    parent::__construct($to);
-  }
-  
-  public function header(): ?string {
-    return $this->header; 
-  }
+    private ButtonAction $action;
 
-  public function body(): string {
-    return $this->body;
-  }
+    public function __construct(string $to, string $body, ButtonAction $action, ?string $header = null, ?string $footer = null)
+    {
+        $this->body = $body;
+        $this->action = $action;
+        $this->header = $header;
+        $this->footer = $footer;
 
-  public function action(): ButtonAction {
-    return $this->action;
-  }
+        parent::__construct($to);
+    }
 
-  public function footer(): ?string {
-    return $this->footer;
-  }
+    public function header(): ?string
+    {
+        return $this->header;
+    }
 
+    public function body(): string
+    {
+        return $this->body;
+    }
+
+    public function action(): ButtonAction
+    {
+        return $this->action;
+    }
+
+    public function footer(): ?string
+    {
+        return $this->footer;
+    }
 }
