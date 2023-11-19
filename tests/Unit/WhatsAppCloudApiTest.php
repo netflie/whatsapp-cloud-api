f3b9626c478379c9a2a4a1f8d8a2d3ce66b02eb5
Álex Albarca <alex@albarca.es>
2023-11-19 17:20:33 +0100

    update-default-graph-version-to-18: update default Graph version to v18

--- tests/Unit/WhatsAppCloudApiTest.php
+++ tests/Unit/WhatsAppCloudApiTest.php
@@ -30,7 +30,7 @@ final class WhatsAppCloudApiTest extends TestCase
 {
     use ProphecyTrait;
 
-    private const TEST_GRAPH_VERSION = 'v17.0';
+    private const TEST_GRAPH_VERSION = 'v18.0';
 
     private $whatsapp_app_cloud_api;
     private $client_handler;
