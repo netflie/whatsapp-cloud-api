f776328b829fa3416e37cb902eb38442d6f1ae1a
Bas Schleijpen <bas@dcc.team>
2024-04-26 11:55:19 +0200

    Add enum types that are not in the meta docs, add isMedia function

--- src/Enums/MessageTypeEnum.php
+++ src/Enums/MessageTypeEnum.php
@@ -5,14 +5,31 @@ enum MessageTypeEnum: string
 {
     case AUDIO = 'audio';
     case BUTTON = 'button';
+    case CONTACTS = 'contacts';
     case DOCUMENT = 'document';
     case TEXT = 'text';
     case IMAGE = 'image';
     case INTERACTIVE = 'interactive';
+    case LOCATION = 'location';
     case ORDER = 'order';
     case STICKER = 'sticker';
     case SYSTEM = 'system';
     case UNKNOWN = 'unknown';
     case VIDEO = 'video';
+    case VOICE = 'voice';
+
+    public static function isMedia(string $type): bool
+    {
+        $mediaTypes = [
+            MessageTypeEnum::AUDIO,
+            MessageTypeEnum::DOCUMENT,
+            MessageTypeEnum::IMAGE,
+            MessageTypeEnum::STICKER,
+            MessageTypeEnum::VIDEO,
+            MessageTypeEnum::VOICE,
+        ];
+
+        return in_array($type, $mediaTypes);
+    }
 }
 
