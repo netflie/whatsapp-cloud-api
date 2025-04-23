b9562960d0ee464c8fc1b1192cb4e2205cc342e1
Mayank <mayank@niswey.com>
2025-04-23 18:52:50 +0530

    update

--- src/WebHook/Notification/PhoneUpdateNotification.php
+++ src/WebHook/Notification/PhoneUpdateNotification.php
@@ -4,7 +4,7 @@ namespace Netflie\WhatsAppCloudApi\WebHook\Notification;
 
 use Netflie\WhatsAppCloudApi\WebHook\Notification;
 
-final class PhoneNumberNameUpdateNotification extends Notification
+final class PhoneUpdateNotification extends Notification
 {
     private string $display_phone_number;
     private string $decision;
@@ -14,29 +14,19 @@ final class PhoneNumberNameUpdateNotification extends Notification
         string $id,
         Support\Business $business,
         string $display_phone_number,
-        string $decision,
-        string $requested_verified_name,
         string $received_at
     ) {
         parent::__construct($id, $business, $received_at);
 
         $this->display_phone_number = $display_phone_number;
-        $this->decision = $decision;
-        $this->requested_verified_name = $requested_verified_name;
     }
 
-    public function displayPhoneNumber(): string
+    public function displayName(Phone\Name $name): self
     {
-        return $this->display_phone_number;
-    }
+        $this->display_phone_number = $name->displayPhoneNumber();
+        $this->decision = $name->decision();
+        $this->requested_verified_name = $name->requestedVerifiedName();
 
-    public function decision(): string
-    {
-        return $this->decision;
-    }
-
-    public function requestedVerifiedName(): string
-    {
-        return $this->requested_verified_name;
+        return $this;
     }
 }
--- src/WebHook/NotificationFactory.php
+++ src/WebHook/NotificationFactory.php
@@ -6,13 +6,13 @@ final class NotificationFactory
 {
     private Notification\MessageNotificationFactory $message_notification_factory;
     private Notification\StatusNotificationFactory $status_notification_factory;
-    private Notification\PhoneNumberNameUpdateNotificationFactory $phone_number_name_update_factory;
+    private Notification\PhoneUpdateNotificationFactory $phone_update_factory;
 
     public function __construct()
     {
         $this->message_notification_factory = new Notification\MessageNotificationFactory();
         $this->status_notification_factory = new Notification\StatusNotificationFactory();
-        $this->phone_number_name_update_factory = new Notification\PhoneNumberNameUpdateNotificationFactory();
+        $this->phone_update_factory = new Notification\PhoneUpdateNotificationFactory();
     }
 
     public function buildFromPayload(array $payload): ?Notification
@@ -57,8 +57,8 @@ final class NotificationFactory
                     $notifications[] = $this->status_notification_factory->buildFromPayload($metadata, $status);
                 }
 
-                if (!empty($field) && $field === 'phone_number_name_update') {
-                    $notifications[] = $this->phone_number_name_update_factory->buildFromPayload($value, $timestamp, $id);
+                if ($field) {
+                    $notifications[] = $this->phone_update_factory->buildFromPayload($value, $timestamp, $id, $field);
                 }
             }
         }
